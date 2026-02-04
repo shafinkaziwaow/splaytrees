425a3aee9cd018b8c7816a89904d0ea3e6f8d650
shafin kazi <shafinkazi5@gmail.com>
2026-02-04 10:50:05 -0500

    made js stuff uhhghghgh

---
+++ Splay/TreeNode.py
@@ -0,0 +1,15 @@
+class TreeNode:
+    
+    def __init__(self, data, x, y):
+        self.right = right
+        self.left = left
+        self.data = data
+        self.x = x
+        self.y = y
+        
+    left = TreeNode()
+    right = TreeNode()
+    
+    
+        
+    
